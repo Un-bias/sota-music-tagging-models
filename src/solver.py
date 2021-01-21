91aa474da12fa7697a74183c2bb5ca901b31f3f5
mathigatti <mathigatti@gmail.com>
2021-01-21 22:00:37 +0000

    genres

--- src/solver.py
+++ src/solver.py
@@ -46,7 +46,7 @@ def read_file(tsv_file):
         for row in reader:
             track_id = row[0]
             tracks[track_id] = {
-                'path': row[3].replace('.mp3', '.npy'),
+                'path': row[3].replace('.mp3', '.npy').replace(".m4a", '.npy'),
                 'tags': row[5:],
             }
     return tracks
@@ -259,8 +259,14 @@ class Solver(object):
         return x
 
     def get_auc(self, est_array, gt_array):
-        roc_aucs  = metrics.roc_auc_score(gt_array, est_array, average='macro')
-        pr_aucs = metrics.average_precision_score(gt_array, est_array, average='macro')
+        try:
+            roc_aucs  = metrics.roc_auc_score(gt_array, est_array, average='macro')
+        except:
+            roc_aucs = 1.
+        try:
+            pr_aucs = metrics.average_precision_score(gt_array, est_array, average='macro')
+        except:
+            pr_aucs = 1.
         print('roc_auc: %.4f' % roc_aucs)
         print('pr_auc: %.4f' % pr_aucs)
         return roc_aucs, pr_aucs
